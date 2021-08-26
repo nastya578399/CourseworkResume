f4440f438a941fd38cd9ab52508286045b5ae660
Romanova Nastya <opt57839@mail.ru>
2021-08-26 12:19:30 +0300

    first commit

---
+++ App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container column">
+    <app-list></app-list>
+
+    <div class="card card-w70">
+      <h1 v-for="item in title" :key="item">{{ item }}</h1>
+      <div class="avatar">
+        <img src="https://cdn.dribbble.com/users/5592443/screenshots/14279501/drbl_pop_r_m_rick_4x.png">
+      </div>
+      <h2>Опыт работы</h2>
+      <p>
+        главный герой американского мультсериала «Рик и Морти», гениальный учёный, изобретатель, атеист (хотя в некоторых сериях он даже молится Богу, однако, каждый раз после чудесного спасения ссылается на удачу и вновь отвергает его существование), алкоголик, социопат, дедушка Морти. На момент начала третьего сезона ему 70 лет[1]. Рик боится пиратов, а его главной слабостью является некий - "Санчезиум". Исходя из того, что существует неограниченное количество вселенных, существует неограниченное количество Риков, герой сериала предположительно принадлежит к измерению С-137. В серии комикcов Рик относится к измерению C-132, а в игре «Pocket Mortys» — к измерению C-123[2]. Прототипом Рика Санчеза является Эмметт Браун, герой кинотрилогии «Назад в будущее»[3].
+      </p>
+      <h3>Добавьте первый блок, чтобы увидеть результат</h3>
+    </div>
+
+
+  </div>
+  <div class="container">
+    <p>
+      <button class="btn primary">Загрузить комментарии</button>
+    </p>
+    <div class="card">
+      <h2>Комментарии</h2>
+      <ul class="list">
+        <li class="list-item">
+          <div>
+            <p><strong>test@microsoft.com</strong></p>
+            <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, reiciendis.</small>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="loader"></div>
+  </div>
+</template>
+
+<script>
+import AppList from './AppList.vue'
+
+export default {
+  data() {
+    return {
+      title: '',
+      subtitle: '',
+      avatar: '',
+      text: '',
+    }
+  },
+  components: {AppList}
+
+}
+</script>
+
+<style>
+  .avatar {
+    display: flex;
+    justify-content: center;
+  }
+
+  .avatar img {
+    width: 150px;
+    height: auto;
+    border-radius: 50%;
+  }
+</style>
---
+++ AppList.vue
@@ -0,0 +1,57 @@
+<template>
+    <form class="card card-w30" @submit.prevent="submitHandler">
+        <div class="form-control">
+            <label for="type" >Тип блока</label>
+            <select id="type" v-model="type">
+                <option value="title">Заголовок</option>
+                <option value="subtitle">Подзаголовок</option>
+                <option value="avatar">Аватар</option>
+                <option value="text">Текст</option>
+            </select>
+        </div>
+
+        <div class="form-control">
+            <label for="value">Значение</label>
+            <textarea 
+            id="value" 
+            rows="3"
+            v-model.trim="text"
+            ></textarea>
+        </div>
+
+        <button type="submit" class="btn primary">Добавить</button>
+    </form>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        subtitle: String,
+        avatar: Image,
+        text: String
+    },
+    data() {
+        return {
+            type: 'title',
+            text: '',
+        }
+    },
+
+    methods: {
+        submitHandler(event) {
+            console.group('Text for summary')
+            console.log('Text:', this.text);
+            console.groupEnd()
+        },
+        newItem() {
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
